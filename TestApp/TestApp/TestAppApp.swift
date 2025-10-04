75af91b1454b6584e5f6a14f698b59be3359e8c5
Даша Суворова <dasha@192.168.100.4>
2025-10-04 12:43:48 +0300

    add project

---
+++ TestApp/TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Даша Суворова on 4.10.25.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
